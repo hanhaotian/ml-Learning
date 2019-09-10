30a0139c3b5113d37d5077d3a15416dd652ba0cd
hanhaotian <herman@stu.xjtu.edu.cn>
2019-09-10 13:39:15 +0800

    Update knn_learning0.py

--- ML Practice/Meachine Learning in Action/knn_learning0.py
+++ ML Practice/Meachine Learning in Action/knn_learning0.py
@@ -2,15 +2,6 @@ from numpy import *
 import operator
 
 
-def createDataSet():
-    group = array([[1.0, 1.1], [1.0, 1.0], [0.0, 0.0], [0, 0.1]])
-    labels = ['A', 'A', 'B', 'B']
-    return group, labels
-
-
-group,labels = createDataSet()
-
-
 def classify_KNN(inX, dataSet, labels, K):
     # shape 表述了数据集的维度
     dataSetSize = dataSet.shape[0]
@@ -27,4 +18,59 @@ def classify_KNN(inX, dataSet, labels, K):
     return sortedClassCount[0][0]
 
 
-print(classify_KNN([0, 0], group, labels, 3))
+def file2matrix(fileName,dim2):
+    fr = open(fileName)
+    arrayOLines = fr.readlines()
+    numberOfLines = arrayOLines.__len__()
+    returnMat = zeros((numberOfLines, dim2))
+    classLabelVector = []
+    fr = open(fileName)
+    index = 0
+    for line in fr.readlines():
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index, :] = listFromLine[0:dim2]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat, classLabelVector
+
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+
+    ranges = maxVals - minVals
+
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m, 1))
+    normDataSet = normDataSet/tile(ranges, (m, 1))
+    return normDataSet, ranges, minVals
+
+
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat, datingLabels = file2matrix("../../../data/data4modle.json", 13)
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m * hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify_KNN(normMat[i, :], normMat[numTestVecs: m, :], datingLabels[numTestVecs: m], 3)
+        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i]))
+        if (classifierResult != datingLabels[i]) : errorCount += 1.0
+    print("the total error rate is: %f" % (errorCount/float(numTestVecs)))
+
+
+datingClassTest()
+
+
+def classifyQuery():
+    resultList = ["not a video", "is a video"]
+    datingDataMat, datingLabels = file2matrix("../../../data/data4modle.json", 13)
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    inArr = array([12, 0, 0, 0, 0, 1, 1, 0, 0.0, 12.0, 82, 0, 0])
+    classifierResult = classify_KNN((inArr - minVals)/ranges, normMat, datingLabels, 3)
+    print("this is a:", resultList[classifierResult - 1])
+
+# classifyQuery()
